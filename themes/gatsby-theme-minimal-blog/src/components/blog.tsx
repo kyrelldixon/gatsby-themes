ebad6917fdade786fd18c0bdeff9284dd2b56b56
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-02-24 10:41:32 -0600

    feat: add basic blog hero

--- themes/gatsby-theme-minimal-blog/src/components/blog.tsx
+++ themes/gatsby-theme-minimal-blog/src/components/blog.tsx
@@ -21,17 +21,34 @@ type PostsProps = {
 }
 
 const Blog = ({ posts }: PostsProps) => {
-  const { tagsPath, basePath } = useMinimalBlogConfig()
+  // const { tagsPath, basePath } = useMinimalBlogConfig()
 
   return (
     <Layout>
       <SEO title="Blog" />
-      <Flex sx={{ alignItems: `center`, justifyContent: `space-between`, flexFlow: `wrap` }}>
+      <div sx={{ p: { margin: 0 }, textAlign: `center` }}>
+        <p sx={{ color: `gray.7` }}>Discover more on my</p>
+        <h2 sx={{ margin: 0, fontSize: [6], fontWeight: `800` }}>Blog</h2>
+        <p sx={{ mb: 2 }}>
+          Get an exclusive behind the scenes look into my mind and stuff by joining my blog. Sign up to receive my
+          monthly newsletter, so you never miss a new article.
+        </p>
+        <form>
+          <label sx={{ color: `primary` }} htmlFor="email">
+            Email
+          </label>
+          <input type="email" name="email" id="email" placeholder="your@email.com" sx={{ pt: 2, pb: 1, py: 2 }} />
+          <button type="submit" sx={{ backgroundColor: `primary`, color: `white`, px: 2, py: 1 }}>
+            Subscribe
+          </button>
+        </form>
+      </div>
+      {/* <Flex sx={{ alignItems: `center`, justifyContent: `space-between`, flexFlow: `wrap` }}>
         <Styled.h2>Blog</Styled.h2>
         <Styled.a as={Link} sx={{ variant: `links.secondary` }} to={replaceSlashes(`/${basePath}/${tagsPath}`)}>
           View all tags
         </Styled.a>
-      </Flex>
+      </Flex> */}
       <Listing posts={posts} sx={{ mt: [4, 5] }} />
     </Layout>
   )

a7cf6c038a66d80cc98ecfa219477184de62cab2
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-02-24 16:14:59 -0600

    feat: add finishing touches to blog styles

--- themes/gatsby-theme-minimal-blog/src/components/blog.tsx
+++ themes/gatsby-theme-minimal-blog/src/components/blog.tsx
@@ -22,7 +22,7 @@ const Blog = ({ posts }: PostsProps) => {
   return (
     <Layout>
       <SEO title="Blog" />
-      <div className="text-center max-w-lg mx-auto mb-4">
+      <div className="text-center max-w-lg mx-auto mb-16 md:mb-24">
         <p className="text-md opacity-50 mb-3 md:mb-6">Discover more of my</p>
         <h2 className="text-5xl font-bold mb-6 text-black leading-10 md:text-6xl md:mb-8">Articles</h2>
         <p className="text-lg mb-4 md:text-xl md:mb-6">
@@ -42,13 +42,7 @@ const Blog = ({ posts }: PostsProps) => {
           </button>
         </form>
       </div>
-      {/* <Flex sx={{ alignItems: `center`, justifyContent: `space-between`, flexFlow: `wrap` }}>
-        <Styled.h2>Blog</Styled.h2>
-        <Styled.a as={Link} sx={{ variant: `links.secondary` }} to={replaceSlashes(`/${basePath}/${tagsPath}`)}>
-          View all tags
-        </Styled.a>
-      </Flex> */}
-      <Listing posts={posts} sx={{ mt: [4, 5] }} />
+      <Listing className="max-w-md mx-auto md:max-w-lg lg:max-w-2xl md:text-center" posts={posts} />
     </Layout>
   )
 }

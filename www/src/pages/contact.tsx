da8c41116516822fbb3e945c811d279e63300b6f
Kyrell Dixon <kyrell@kyrelldixon.com>
2020-02-25 18:47:40 -0600

    feat: add contact hero

--- www/src/pages/contact.tsx
+++ www/src/pages/contact.tsx
@@ -5,25 +5,26 @@ import SEO from "@lekoarts/gatsby-theme-minimal-blog/src/components/seo"
 const ContactPage = () => (
   <Layout>
     <SEO title="Contact" />
-    <div className="text-center max-w-lg mx-auto mb-16 md:mb-24">
-      <p className="text-md opacity-50 mb-3 md:mb-6">What are you working on?</p>
-      <h2 className="text-5xl font-bold mb-6 text-black leading-10 md:text-6xl md:mb-8">Let's talk about you</h2>
-      <p className="text-lg mb-4 md:text-xl md:mb-6">
+    <div className="text-center flex flex-col items-center max-w-lg md:max-w-2xl mx-auto mb-16 md:mb-24">
+      <p className="text-md opacity-50 mb-3 md:md-2">What are you working on?</p>
+      <h2 className="text-5xl font-bold mb-4 text-black leading-tight md:md-6 md:text-6xl ">
+        Let's talk
+        <br className="md:hidden" /> about you
+      </h2>
+      <p className="text-md mb-6 md:text-xl md:mb-6">
         If you have a project in mind or you just want to chat, feel free to get in touch. Currently my time books
         quickly, so the sooner you write, the better it is for both of us.
       </p>
-      <form className="flex justify-between shadow-md max-w-sm mx-auto">
-        <input
-          id="email"
-          className="px-4 py-2 flex-grow rounded-l-sm"
-          type="email"
-          name="email"
-          placeholder="your@email.com"
-        />
-        <button className="text-white px-4 py-2 rounded-r-sm bg-primary" type="submit">
-          Subscribe
-        </button>
-      </form>
+      <p className="text-sm mb-8 flex items-center tracking-wide">
+        <span className="inline-block h-3 w-3 rounded-full bg-green-300 shadow-md mr-2" />
+        <span className="font-semibold mr-1">Reply time:</span>within 6-12 hours
+      </p>
+      <a
+        href="mailto:hello@kyrelldixon.com"
+        className="text-white text-sm font-semibold px-6 py-3 rounded-sm shadow-blue bg-primary"
+      >
+        hello@kyrelldixon.com
+      </a>
     </div>
   </Layout>
 )
